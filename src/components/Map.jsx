e2577a7335fed5ecabbe7c26ad5a71b64d5dc774
unknown <me.leandrop@gmail.com>
2023-07-15 19:24:40 -0300

    location responsive

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import mapa from "../assets/mapa.png";
 
 function Map() {
   return (
-    <div className="w-1/4">
+    <div className="w-4/4 z-0">
       <a
         href="https://www.google.com/maps/place/La+Guadalupe/@-33.0468159,-68.8083339,17z/data=!3m1!4b1!4m6!3m5!1s0x967e0b1a81309d4b:0x588836c1de0b0390!8m2!3d-33.0468159!4d-68.8083339!16s%2Fg%2F11fj9c41ph?entry=ttu"
         target="_blank"
@@ -13,7 +12,7 @@ function Map() {
         <img
           src={mapa}
           alt="Map"
-          className="object-cover max-w-full h-full"
+          className="object-cover max-w-full h-full px-2"
         />
       </a>
     </div>
--- src/sections/Location.jsx
+++ src/sections/Location.jsx
@@ -1,10 +1,33 @@
 import Map from "../components/Map";
+import { planta, planta2, planta3, planta4 } from "../constants";
 
 export const Location = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center">
-      <h1>Ubicación</h1>
+    <div className="lg:w-6/12 w-full flex flex-col lg:gap-5 items-center justify-center py-10 bg-blue z-50 relative overflow-hidden">
+
+      <h1 className="text-4xl lg:text-7xl font-black text-white z-10 py-2">Cómo llegar:</h1>
+      <h1 className="text-xl lg:text-2xl font-black text-lila z-10 ">click en el mapa</h1>
       <Map />
+
+      
+    <div className="hidden lg:block">
+     <img src={planta} alt="" className="absolute -z-10 -bottom-40 -right-20 rotate-12 -scale-100"/>
+
+     <img src={planta2} alt="" className="absolute -z-10 -left-60 bottom-20"/>
+     <img src={planta2} alt="" className="absolute -z-10 -right-60 bottom-20 rotate-180"/>
+
+     <img src={planta3} alt="" className="absolute -z-10 -bottom-20 -left-80 -rotate-45"/>
+
+     <img src={planta4} alt="" className="absolute -z-10 -top-40 -right-40 -rotate-45 opacity-50"/>
+     <img src={planta4} alt="" className="absolute -z-10 -top-60 -left-40 -rotate-45 -scale-100 opacity-50"/>
+    
+    </div>
+    <div className="block lg:hidden">
+    <img src={planta} alt="" className="absolute -z-10 -bottom-20 -right-20 rotate-12 -scale-100"/>
+    <img src={planta} alt="" className="absolute -z-10 -top-40 -right-20 rotate-12 -scale-100"/>
+
+    </div>
+    
     </div>
   );
 };
