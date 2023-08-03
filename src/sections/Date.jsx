86972d38171420a3f14950baa45d754f44c2ce91
unknown <me.leandrop@gmail.com>
2023-08-02 23:11:33 -0300

    date section

---
+++ src/sections/Date.jsx
@@ -0,0 +1,10 @@
+const Date = () => {
+  return (
+    <div className="flex flex-col text-center lg:flex-row items-center justify-center text-3xl font-bold gap-5 bg-black text-lilaclaro p-5">
+      <h4>Festejamos nuestro amor el </h4>
+      <h4 className="text-white text-4xl">Sábado 21 de Octubre</h4>
+    </div>
+  );
+};
+
+export default Date;
