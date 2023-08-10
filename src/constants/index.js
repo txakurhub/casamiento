f20ababb706564d24af328fcaccff82bc971d9eb
unknown <me.leandrop@gmail.com>
2023-08-10 18:33:20 -0300

    fix import

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,6 @@ import planta4 from "../assets/image4.png";
 import planta5 from "../assets/image5.png";
 import curve from "../assets/curve.png"
 import circlebg from "../assets/circle-bg.png"
-import flores from "../assets/flores.png"
 const lila = "#471C95";
 
-export { planta, planta1, planta2, planta3, planta4, planta5, lila, curve, circlebg, flores };
+export { planta, planta1, planta2, planta3, planta4, planta5, lila, curve, circlebg };
